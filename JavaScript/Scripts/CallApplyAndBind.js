dba4a378d35fcb2b5208ccef66fcdb57771f2f37
tr1ple5 <DerrickE@octitle.com>
2016-09-13 16:59:16 -0700

    did more tutorials

--- JavaScript/Scripts/CallApplyAndBind.js
+++ JavaScript/Scripts/CallApplyAndBind.js
@@ -16,4 +16,15 @@ var addToThis = function (a,b,c) {
     return this.num + a + b + c;
 
 };
+//call
 console.log(addToThis.call(obj, 3, 2, 1)); // functionName.call(obj, function arguments)
+
+//apply
+var array = [3, 2, 1];
+console.log(addToThis.apply(obj, array)); // functionName.apply(obj, function ARRAY arguments)
+
+
+//bind
+var bound = addToThis.bind(obj);
+
+console.log(bound(3, 2, 1));
---
+++ JavaScript/Scripts/CallBackFunctions.js
@@ -0,0 +1,50 @@
+﻿
+/*
+let x = function () {
+    console.log("i am called from inside the function");
+};
+
+let y = function (callback) {
+    console.log("do something");
+    callback();
+}
+
+y(x);
+*/
+
+/*
+// doing something without call back function
+let calc = function (num1, num2, calcType) {
+
+    if (calcType.toLowerCase() === "add") {
+        return num1 + num2;
+    } else if (calcType.toLowerCase() === "multiply"){
+        return num1 * num2;
+    }
+};
+
+
+console.log(calc(2, 3, "Add"));
+*/
+
+//"better way to accomplish this is:
+
+let add = function(a, b) {
+    return a + b;
+};
+
+let multiply = function (a, b) {
+    return a * b;
+};
+
+let doWhatever = function (a, b) {
+    console.log("here are your two numbers back: ${a}, ${b}");
+};
+
+let calc = function (num1, num2, callback) {
+    return callback(num1, num2);
+};
+
+console.log(calc(2, 3, add));
+console.log(calc(2, 3, multiply));
+console.log(calc(2, 3, doWhatever));
